0bc05f78d0e49437d179036600106090bf3f3f8b
Kester <109535119+K3ster@users.noreply.github.com>
2024-11-14 08:42:54 +0000

    Cm3_3_2

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const apiKey = "MoagLlguAnOp4i6PnC22FBWKST7fHw9d";
 
-async function fetchRandomGif() {
+async function searchGifs() {
+  const query = document.getElementById("searchQuery").value;
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&rating=g`,
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10&rating=g`,
   );
   const data = await response.json();
-  const gifUrl = data.data.images.original.url;
-
   const gifContainer = document.getElementById("gifContainer");
-  gifContainer.innerHTML = `<img src="${gifUrl}" alt="Random GIF">`;
+  gifContainer.innerHTML = ""; // Clear existing data
+
+  data.data.forEach((gif) => {
+    const gifUrl = gif.images.original.url;
+    gifContainer.innerHTML += `<img src="${gifUrl}" alt="GIF" style="margin: 10px;">`;
+  });
 }
