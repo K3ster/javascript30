5bd8e51e8c0c0f7433d3a37112bbaffe6aa9242d
Kester <109535119+K3ster@users.noreply.github.com>
2024-11-14 08:46:15 +0000

    Cm3_3_3

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const apiKey = "MoagLlguAnOp4i6PnC22FBWKST7fHw9d";
+let currentPage = 1;
 
-async function searchGifs() {
+async function searchGifs(page) {
   const query = document.getElementById("searchQuery").value;
+  const offset = (page - 1) * 10;
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10&rating=g`,
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=1&offset=${offset}&rating=g`,
   );
   const data = await response.json();
   const gifContainer = document.getElementById("gifContainer");
@@ -13,4 +15,16 @@ async function searchGifs() {
     const gifUrl = gif.images.original.url;
     gifContainer.innerHTML += `<img src="${gifUrl}" alt="GIF" style="margin: 10px;">`;
   });
+
+  currentPage = page;
+}
+
+function nextPage() {
+  searchGifs(currentPage + 1);
+}
+
+function previousPage() {
+  if (currentPage > 1) {
+    searchGifs(currentPage - 1);
+  }
 }
